041659efddda9d8066e9cc8082eaf6f597cb941a
John Lester Regalado <janjanregalado1@gmail.com>
2024-04-12 11:10:20 +0800

    download cv nalang kulang sa portfolio

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,10 @@ import { Link } from "react-scroll";
 
 const Contact = () => {
   return (
-    <div className="h-screen lg:mt-0 mt-[110%] flex flex-col items-center justify-evenly" id="contact">
+    <div
+      className="h-screen lg:mt-0 mt-[110%] flex flex-col items-center justify-evenly"
+      id="contact"
+    >
       <div>
         <div className="flex flex-col text-center gap-1">
           <span className="text-md font-medium text-purple-800">
@@ -13,20 +16,34 @@ const Contact = () => {
 
         <div className="w-auto border-2 border-slate-900 mt-[10%] p-4 flex justify-center gap-5 rounded-3xl">
           <div className="flex gap-2">
-            <span className="material-symbols-outlined text-lg cursor-pointer">
-              mail
-            </span>
-            <span className="text-md font-semibold cursor-pointer">
-              janjanregalado1@gmail.com
-            </span>
+            <a href="mailto:janjanregalado1@gmail.com">
+              <span className="material-symbols-outlined text-lg cursor-pointer">
+                mail
+              </span>
+            </a>
+            <a href="mailto:janjanregalado1@gmail.com">
+              <span className="text-md font-semibold cursor-pointer">
+                janjanregalado1@gmail.com
+              </span>
+            </a>
           </div>
           <div className="flex gap-2">
-            <span className="material-symbols-outlined text-lg cursor-pointer">
-              mail
-            </span>
-            <span className="text-md font-semibold cursor-pointer">
-              LinkedIn
-            </span>
+            <a
+              href="https://www.linkedin.com/in/john-lester-regalado-147847271/"
+              target="_blank"
+            >
+              <span className="material-symbols-outlined text-lg cursor-pointer">
+                mail
+              </span>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/john-lester-regalado-147847271/"
+              target="_blank"
+            >
+              <span className="text-md font-semibold cursor-pointer">
+                LinkedIn
+              </span>
+            </a>
           </div>
         </div>
       </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,17 +6,30 @@ const Main = () => {
   return (
     <main className="md:flex mt-[10%] gap-20 justify-center">
       <div className="flex justify-center">
-        <img src={PROFILE} alt="Profile" className="h-80 rounded-[50%]"/>
+        <img src={PROFILE} alt="Profile" className="h-80 rounded-[50%]" />
       </div>
       <div className="flex flex-col gap-1 text-center mt-16">
         <span className="text-lg font-normal">Hello, I'm</span>
         <span className="text-3xl font-bold">John Lester Regalado</span>
         <span className="text-2xl font-normal">Full Stack Web Developer</span>
         <div className="flex gap-3 mt-3 justify-center">
-          <button className="border-2 border-black p-3 rounded-3xl text-sm font-bold hover:bg-black hover:text-white">Download CV</button>
+          <button className="border-2 border-black p-3 rounded-3xl text-sm font-bold hover:bg-black hover:text-white">
+            Download CV
+          </button>
           <div className="flex gap-2 mt-2">
-            <img src={LINKEDIN} alt="linked-in" className="h-8 cursor-pointer"/>
-            <img src={GITHUB} alt="github" className="h-8 cursor-pointer"/>
+            <a
+              href="https://www.linkedin.com/in/john-lester-regalado-147847271/"
+              target="_blank"
+            >
+              <img
+                src={LINKEDIN}
+                alt="linked-in"
+                className="h-8 cursor-pointer"
+              />
+            </a>
+            <a href="https://github.com/Kixx723" target="_blank">
+              <img src={GITHUB} alt="github" className="h-8 cursor-pointer" />
+            </a>
           </div>
         </div>
       </div>
